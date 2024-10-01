703ad67d2f979e06ffd528c7b16525e13b715e3f
koushikgeddam <84722351+koushikgeddam@users.noreply.github.com>
2024-10-02 03:13:56 +0530

    Add files via upload
    Signed-off-by: koushikgeddam <84722351+koushikgeddam@users.noreply.github.com>

--- signup.php
+++ signup.php
@@ -2,17 +2,22 @@
 if (isset($_POST["submit"])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $password = $_POST['password'];  // New password field added
+    $password = $_POST['password'];
 
     $from = 'sender@mail';  // Email that sends the signup info
     $to = 'honey1432koushik@gmail.com';  // Recipient email address
-    $subject = 'Signup Form Submission';  // Email subject
+    $subject = 'Signup Form Submission';
 
     // Message body for the email
     $body = "From: $name\nE-Mail: $email\nPassword: $password\n";
 
+    // Email headers
+    $headers = "From: $from\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
     // Sending email
-    if (mail($to, $subject, $body, "From: $from")) {
+    if (mail($to, $subject, $body, $headers)) {
         header("Location: thank-you.html");  // Redirect to a thank you page after success
         exit;
     } else {
